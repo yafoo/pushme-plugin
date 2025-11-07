89096068758596b7e0821d29674e0cac7c32dac6
yafu <zhyaphoo@qq.com>
2025-11-07 18:15:03 +0800

    优化智能主题插件

--- example/auto_theme.js
+++ example/auto_theme.js
@@ -1,6 +1,6 @@
 /**
 * @name 智能主题
-* @description 根据消息标题文字，自动添加相应主题
+* @description 根据消息标题文字，自动设置主题
 * @param {Object} msg - 消息体
 * @param {String} msg.title - 消息标题
 * @param {String} msg.content - 消息内容
@@ -9,26 +9,16 @@
 * @param {Function} next - 下一个插件
 */
 function(msg, next) {
-    if(!~['', 'text', 'markdown'].indexOf(msg.type)) {
-        return next();
-    }
-    const reg = /^\[[iswf]\]/;
-    if(!reg.test(msg.title)) {
-        const keys = {
-            f: ['失败', '错误', '异常', '失效', '过期', '不存在'],
-            w: ['告警', '警告', '通知', '提示', '提醒'],
-            s: ['成功', '完成'],
-            i: ['消息', '信息']
-        };
-        let is_match = false;
-        Object.entries(keys).forEach(([k, list]) => {
-            list.forEach(value => {
-                if(!is_match && msg.title.includes(value)) {
-                    msg.title = '[' + k + ']' + msg.title;
-                    is_match = true;
-                }
-            });
-        });
-    }
+    if(!['', 'text', 'markdown', 'html'].includes(msg.type)) return next();
+    if(/^\[[iswf]\]/.test(msg.title)) return next();
+    
+    const theme = Object.entries({
+        f: ['失败', '错误', '异常', '失效', '过期', '不存在'],
+        w: ['告警', '警告', '通知', '提示', '提醒'],
+        s: ['成功', '完成'],
+        i: ['消息', '信息']
+    }).find(([_, words]) => words.some(word => msg.title.includes(word)))?.[0];
+    
+    if(theme) msg.title = `[${theme}]${msg.title}`;
     next();
 }
