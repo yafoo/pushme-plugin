af3c8b17a8b0140ec2f0a7942a6037cfce50fccc
yafu <zhyaphoo@qq.com>
2025-11-18 16:45:10 +0800

    RSA加解密示例

---
+++ example/rsa_demo.js
@@ -0,0 +1,20 @@
+/**
+ * @name RSA加解密示例
+ * @param {Object} msg - 消息对象
+ * @param {String} msg.title - 标题
+ * @param {String} msg.content - 内容
+ * @param {String} msg.date - 时间
+ * @param {String} msg.type - 类型
+ * @param {Function} next - 下一个插件
+ * @dependencies PMF.toast、PMF.rsa
+ */
+function(msg, next) {
+    const key = PMF.rsa.getPublicKey();
+
+    msg.title = PMF.rsa.encrypt(msg.title, key)
+    PMF.toast('加密：' + msg.title)
+
+    msg.title = PMF.rsa.decrypt(msg.title, key)
+    PMF.toast('解密：' + msg.title)
+    next();
+}
